76796ed89e626faa222a0cc08caac59d52bd8756
siddharthshiv <sidshiv.photography@gmail.com>
2021-08-07 20:10:49 +0100

    added a file to interact with socket.io in the front end

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 //Importing Express
 const express = require('express');
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//This code is run when a user connects
+
+io.on('connection', (socket) =>{
+
+    console.log("New user connected to the socket ... ");
+});
 
 const PORT = process.env.PORT || 3000;
 
